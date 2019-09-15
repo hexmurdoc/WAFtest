bd41b3af23a09c82aa472ead1b6067c54f480d11
amir.shladovsky <amir.shladovsky@imperva.com>
2019-09-15 12:01:41 +0300

    fix script

--- attackPayloads.py
+++ attackPayloads.py
@@ -5,8 +5,9 @@ import sys
 
 
 host = sys.argv[1]
-q_params = 'a=cmd.exe /c ping 1.1.1.1'
-res  = requests.get(host+'?'+q_params)
+q_params = 'command=cmd.exe /C ping 1.1.1.1'
+print('issue request to:'+ host+'/?'+q_params)
+res  = requests.get('http://'+host+'/?'+q_params)
 
 if res.status_code == 200 and 'incident' not in res.text :
     print("Passed the WAF")
